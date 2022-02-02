454d035a7b2774cc6db52f1d50ae1ce188f1b59d
GrafSoul <grafsoul@gmail.com>
2022-02-02 19:52:40 +0500

    feat: Update file in the  project

--- popup.js
+++ popup.js
@@ -1,18 +1,48 @@
 const timeElement = document.getElementById("time");
 const nameElement = document.getElementById("name");
-const currentTime = new Date().toLocaleTimeString();
-timeElement.textContent = `The time is: ${currentTime}`;
+const timerElement = document.getElementById("timer");
 
-chrome.action.setBadgeText(
-  {
-    text: "TIME",
-  },
-  () => {
-    console.log("Finished settings badge text");
-  }
-);
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+const resetBtn = document.getElementById("reset");
+
+function updateTimeElements() {
+  chrome.storage.local.get(["timer"], (res) => {
+    const time = res.timer ?? 0;
+    timerElement.textContent = `The timer is at: ${time} seconds`;
+  });
+
+  const currentTime = new Date().toLocaleTimeString();
+  timeElement.textContent = `The time is: ${currentTime}`;
+}
+
+updateTimeElements();
+setInterval(updateTimeElements, 1000);
 
 chrome.storage.sync.get(["name"], (res) => {
   const name = res.name ?? "???";
   nameElement.textContent = `You name is: ${name}`;
+});
+
+startBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: true,
+  });
+});
+
+stopBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    isRunning: false,
+  });
+});
+
+resetBtn.addEventListener("click", () => {
+  chrome.storage.local.set({
+    timer: 0,
+    isRunning: false,
+  });
+
+  chrome.action.setBadgeText({
+    text: ` ${0} `,
+  });
 });
